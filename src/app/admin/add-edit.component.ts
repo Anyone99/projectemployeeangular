d960a54be4c2237ea12dcf3be19e376e93d0e270
Anyone99 <yangmi1998@gmail.com>
2020-05-07 18:17:15 +0200

    funciona calcular dia de vacaciones

--- src/app/admin/add-edit.component.ts
+++ src/app/admin/add-edit.component.ts
@@ -1,114 +1,138 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { first } from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { first } from "rxjs/operators";
 
-import { AccountService, AlertService } from '../_services';
+import { AccountService, AlertService } from "../_services";
 
-@Component({ templateUrl: 'add-edit.component.html' })
+@Component({ templateUrl: "add-edit.component.html" })
 export class AddEditComponent implements OnInit {
-    form: FormGroup;
-    id: string;
-    isAddMode: boolean;
-    loading = false;
-    submitted = false;
-    fechaContrato;
-    diaVacaciones;
+  form: FormGroup;
+  id: string;
+  isAddMode: boolean;
+  loading = false;
+  submitted = false;
+  fechaContrato;
+  showDiaVacaciones;
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
-        private router: Router,
-        private accountService: AccountService,
-        private alertService: AlertService
-    ) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private accountService: AccountService,
+    private alertService: AlertService
+  ) {}
 
-    ngOnInit() {
-        this.id = this.route.snapshot.params['id'];
-        this.isAddMode = !this.id;
-        
-        // password not required in edit mode
-        const passwordValidators = [Validators.minLength(6)];
-        if (this.isAddMode) {
-            passwordValidators.push(Validators.required);
-        }
+  ngOnInit() {
+    this.id = this.route.snapshot.params["id"];
+    this.isAddMode = !this.id;
 
-        this.form = this.formBuilder.group({
-            nombre: ['', Validators.required],
-            apellido: ['', Validators.required],
-            dni: ['', Validators.required],
-            password: ['', passwordValidators],
-            fechaContrato : ['', Validators.required],
-            diaVacaciones : [0,Validators.required]
+    // password not required in edit mode
+    const passwordValidators = [Validators.minLength(6)];
+    if (this.isAddMode) {
+      passwordValidators.push(Validators.required);
+    }
+
+    this.form = this.formBuilder.group({
+      nombre: ["", Validators.required],
+      apellido: ["", Validators.required],
+      dni: ["", Validators.required],
+      password: ["", passwordValidators],
+      fechaContrato: ["", Validators.required],
+      diaVacaciones: [0, Validators.required]
+    });
+
+    if (!this.isAddMode) {
+      this.accountService
+        .getById(this.id)
+        .pipe(first())
+        .subscribe(x => {
+          this.f.nombre.setValue(x.nombre);
+          this.f.apellido.setValue(x.apellido);
+          this.f.dni.setValue(x.dni);
+          this.f.fechaContrato.setValue(x.fechaContrato);
+          this.f.diaVacaciones.setValue(x.diaVacaciones);
         });
+    }
+  }
 
-        if (!this.isAddMode) {
-            this.accountService.getById(this.id)
-                .pipe(first())
-                .subscribe(x => {
-                    this.f.nombre.setValue(x.nombre);
-                    this.f.apellido.setValue(x.apellido);
-                    this.f.dni.setValue(x.dni);
-                    this.f.fechaContrato.setValue(x.fechaContrato);
-                    this.f.diaVacaciones.setValue(x.diaVacaciones);
-                });
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.form.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // reset alerts on submit
+    this.alertService.clear();
+
+    // stop here if form is invalid
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    if (this.isAddMode) {
+      this.createUser();
+    } else {
+      this.updateUser();
+    }
+  }
+
+  calcularDiaVaciones(fecha : Date) {
+    console.log(fecha)
+    if (this.fechaContrato) {
+      const mesTrabajo = 2.5;
+      const contrato = new Date(this.fechaContrato);
+      const now = new Date(Date.now());
+      const timeDiff = Math.abs(now.getTime() - contrato.getTime());
+      // days
+      const diffDays = timeDiff / 86400000;
+
+      const diffYear = diffDays / 365;
+
+      const diffMonth = diffYear * 12;
+
+      this.showDiaVacaciones = diffMonth * mesTrabajo;
+
+      console.log(diffDays + " " + this.showDiaVacaciones);
+    }
+  }
+
+  private createUser() {
+    this.accountService
+      .register(this.form.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.alertService.success("User added successfully", {
+            keepAfterRouteChange: true
+          });
+          this.router.navigate([".", { relativeTo: this.route }]);
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false;
         }
-    }
+      );
+  }
 
-    // convenience getter for easy access to form fields
-    get f() { return this.form.controls; }
-
-    onSubmit() {
-        this.submitted = true;
-
-        // reset alerts on submit
-        this.alertService.clear();
-
-        // stop here if form is invalid
-        if (this.form.invalid) {
-            return;
+  private updateUser() {
+    this.accountService
+      .update(this.id, this.form.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.alertService.success("Update successful", {
+            keepAfterRouteChange: true
+          });
+          this.router.navigate(["..", { relativeTo: this.route }]);
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false;
         }
-
-        this.loading = true;
-        if (this.isAddMode) {
-            this.createUser();
-        } else {
-            this.updateUser();
-        }
-    }
-
-    calcularDiaVaciones(){
-      if (this.fechaContrato){
-
-      }
-
-    }
-
-    private createUser() {
-        this.accountService.register(this.form.value)
-            .pipe(first())
-            .subscribe(
-                data => {
-                    this.alertService.success('User added successfully', { keepAfterRouteChange: true });
-                    this.router.navigate(['.', { relativeTo: this.route }]);
-                },
-                error => {
-                    this.alertService.error(error);
-                    this.loading = false;
-                });
-    }
-
-    private updateUser() {
-        this.accountService.update(this.id, this.form.value)
-            .pipe(first())
-            .subscribe(
-                data => {
-                    this.alertService.success('Update successful', { keepAfterRouteChange: true });
-                    this.router.navigate(['..', { relativeTo: this.route }]);
-                },
-                error => {
-                    this.alertService.error(error);
-                    this.loading = false;
-                });
-    }
+      );
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,17 +7,16 @@ import { AdminComponent } from "./admin";
 import { Role } from "./_models";
 const accountModule = () =>
   import("./account/account.module").then(x => x.AccountModule);
-const usersModule = () =>
+const adminModule = () =>
   import("./admin/admin.module").then(x => x.AdminModule);
 
 const routes: Routes = [
   { path: "", component: HomeComponent, canActivate: [AuthGuard] },
-  { path: "employee", canActivate: [AuthGuard] },
+  //{ path: "employee", canActivate: [AuthGuard] },
   { path: "account", loadChildren: accountModule },
   {
-    path: "admin",
-    component: AdminComponent,
-    loadChildren: usersModule,
+    path: "employee",
+    loadChildren: adminModule,
     canActivate: [AuthGuard],
     data: { roles: [Role.Admin] }
   },
